79f31a3a31291cec5b668e6ba95be7e16232f73f
hvanyo <hvanyo@its.jnj.com>
2022-05-23 18:34:27 -0500

    Update Header/Fotoer

--- packages/bodiless-demo/src/components/Templates/GenericTemplate.ts
+++ packages/bodiless-demo/src/components/Templates/GenericTemplate.ts
@@ -12,5 +12,19 @@
  * limitations under the License.
  */
 
-export { default as DemoPage } from './components/Templates/Page';
-export { GitStar } from './components/GitHubButtons';
+// TBD remove after shadowing PR merges....
+
+import {
+  vitalGenericTemplate, asGenericTemplateToken
+} from '@bodiless/vital-templates';
+import { DemoLayout } from '../Layout';
+
+const Default = asGenericTemplateToken(vitalGenericTemplate.Default, {
+  Components: {
+    PageWrapper: DemoLayout,
+  },
+});
+
+export default {
+  Default,
+};
