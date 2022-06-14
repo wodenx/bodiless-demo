5519dd85528413872716fcf5428a30a68ba50ec5
hvanyo <hvanyo@its.jnj.com>
2022-06-14 17:42:33 -0500

    Add Table

---
+++ packages/bodiless-demo/src/components/Tables/index.ts
@@ -0,0 +1,85 @@
+import {
+  addProps, as, on, withDesign, addClassesIf, and, replaceWith, flowHoc,
+} from '@bodiless/fclasses';
+import { withNode, withNodeKey } from '@bodiless/core';
+import { asBodilessChameleon } from '@bodiless/components';
+import {
+  asBodilessTable,
+  useIsInBody,
+  useIsInHead,
+  useIsSecondColumn,
+  useIsThirdColumn,
+  useIsFirstColumn,
+} from '@bodiless/table';
+import { vitalTable, asTableToken } from '@bodiless/vital-table';
+import {
+  EditorPlainClean, RichTextClean, vitalEditorPlain, vitalRichText
+} from '@bodiless/vital-editors';
+import GreenYes from '../../assets/GreenYes';
+import RedNo from '../../assets/RedNo';
+
+const DemoStyleTable = asTableToken({
+  Meta: flowHoc.meta.term('Type')('Table'),
+  Schema: {
+    _: asBodilessTable(),
+    CellContent: as(withNode, withNodeKey('Cell')),
+  },
+  Theme: {
+    Table: 'mx-auto',
+    THead: 'bg-demo-primary-interactive text-white',
+    TFoot: 'bg-demo-primary-interactive',
+    Cell: addClassesIf(and(useIsInBody, useIsFirstColumn))('text-center'),
+  },
+  Spacing: {
+    Cell: 'p-2',
+  },
+  Editors: {
+    CellContent: as(
+      asBodilessChameleon('component', { component: 'Editor' }, () => ({ groupLabel: 'Cell Content', label: 'Type' })),
+      withDesign({
+        Yes: replaceWith(GreenYes),
+        No: replaceWith(RedNo),
+        PlainEditor: on(EditorPlainClean)(vitalEditorPlain.Default, addProps({ placeholder: 'Cell' })),
+        Editor: on(RichTextClean)(vitalRichText.Default, addProps({ placeholder: 'Cell' })),
+      }),
+    ),
+  },
+});
+
+const WithHighlightSecondColumn = asTableToken({
+  Theme: {
+    Cell: as(
+      addClassesIf(and(useIsInBody, useIsSecondColumn))('bg-demo-table-light'),
+      addClassesIf(and(useIsInHead, useIsSecondColumn))('font-extrabold italic'),
+    ),
+  },
+  Meta: flowHoc.meta.term('Style')('Custom Hightlighted: Second'),
+});
+
+const WithHighlightThirdColumn = asTableToken({
+  Theme: {
+    Cell: as(
+      addClassesIf(and(useIsInBody, useIsThirdColumn))('bg-demo-table-light'),
+      addClassesIf(and(useIsInHead, useIsThirdColumn))('font-extrabold italic'),
+    ),
+  },
+  Meta: flowHoc.meta.term('Style')('Custom Hightlighted: Third'),
+});
+
+const asSecondColumnHighlighted = as(
+  DemoStyleTable,
+  vitalTable.WithFlowContainerPreview,
+  vitalTable.WithBorders,
+  WithHighlightSecondColumn,
+);
+const asThirdColumnHighlighted = as(
+  DemoStyleTable,
+  vitalTable.WithFlowContainerPreview,
+  vitalTable.WithBorders,
+  WithHighlightThirdColumn,
+);
+
+export {
+  asSecondColumnHighlighted,
+  asThirdColumnHighlighted,
+};
--- packages/bodiless-demo/src/shadow/@bodiless/vital-flowcontainer/FlowContainer.ts
+++ packages/bodiless-demo/src/shadow/@bodiless/vital-flowcontainer/FlowContainer.ts
@@ -2,8 +2,10 @@ import { on } from '@bodiless/fclasses';
 import { asFluidToken } from '@bodiless/vital-elements';
 import { vitalFlowContainerBase } from '@bodiless/vital-flowcontainer';
 import { CardClean } from '@bodiless/vital-card';
+import { TableClean } from '@bodiless/vital-table';
 import { StyledEditor, StyledEditorClean } from '../../../components/StyledEditor';
 import { asGetStarted } from '../../../components/Card';
+import { asSecondColumnHighlighted, asThirdColumnHighlighted } from '../../../components/Tables';
 
 const ExtraPadding = asFluidToken(vitalFlowContainerBase.Default, {
   Spacing: {
@@ -18,6 +20,8 @@ const Default = asFluidToken(
       ...vitalFlowContainerBase.Default.Components,
       StyledEditorBorder: on(StyledEditorClean)(StyledEditor.Borders),
       GetStarted: on(CardClean)(asGetStarted),
+      SecondHighlightedTable: on(TableClean)(asSecondColumnHighlighted),
+      ThirdHighlightedTable: on(TableClean)(asThirdColumnHighlighted),
     },
   },
   ExtraPadding,
