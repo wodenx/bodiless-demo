7fdf5b54728be90b1bd2d203f5ebea44d2b0c69e
Heidi Vanyo <58534559+hvanyo@users.noreply.github.com>
2022-06-17 13:44:12 -0600

    use static cards (#16)
    * replace cards with static version
    
    * fix card

--- packages/bodiless-demo/src/shadow/@bodiless/vital-flowcontainer/FlowContainer.ts
+++ packages/bodiless-demo/src/shadow/@bodiless/vital-flowcontainer/FlowContainer.ts
@@ -1,7 +1,7 @@
 import { on } from '@bodiless/fclasses';
 import { asFluidToken } from '@bodiless/vital-elements';
 import { vitalFlowContainerBase } from '@bodiless/vital-flowcontainer';
-import { CardClean } from '@bodiless/vital-card';
+import { CardStatic } from '@bodiless/vital-card';
 import { StyledEditor, StyledEditorClean } from '../../../components/StyledEditor';
 import { asGetStarted } from '../../../components/Card';
 import { TitleTableClean, asSecondColumnHighlightedTitle, asThirdColumnHighlightedTitle } from '../../../components/Tables';
@@ -18,7 +18,7 @@ const Default = asFluidToken(
     Components: {
       ...vitalFlowContainerBase.Default.Components,
       StyledEditorBorder: on(StyledEditorClean)(StyledEditor.Borders),
-      GetStarted: on(CardClean)(asGetStarted),
+      GetStarted: on(CardStatic)(asGetStarted),
       SecondHighlightedTable: on(TitleTableClean)(asSecondColumnHighlightedTitle),
       ThirdHighlightedTable: on(TitleTableClean)(asThirdColumnHighlightedTitle),
     },
