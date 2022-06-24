5524a2bd573b179090f424d91d473f793a2284b5
Heidi Vanyo <58534559+hvanyo@users.noreply.github.com>
2022-06-24 12:30:15 -0600

    Update SEO (#21)

--- packages/bodiless-demo/src/shadow/@bodiless/vital-layout/Layout.ts
+++ packages/bodiless-demo/src/shadow/@bodiless/vital-layout/Layout.ts
@@ -1,8 +1,7 @@
 import { asLayoutToken, vitalHeader, vitalLayoutBase } from '@bodiless/vital-layout';
 import asBodilessDemoFooter from '../../../components/Footer';
 
-const Default = asLayoutToken({
-  ...vitalLayoutBase.Base,
+const Default = asLayoutToken(vitalLayoutBase.Base, {
   Components: {
     Header: vitalHeader.Default,
     Footer: asBodilessDemoFooter,
