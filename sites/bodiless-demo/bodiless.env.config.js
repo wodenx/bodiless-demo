cccbe71eee64a9b3c00347dce10884d62314bae9
Chris Oden <coden@its.jnj.com>
2022-07-07 12:58:04 -0400

    Disable save on main branch.

--- sites/bodiless-demo/bodiless.env.config.js
+++ sites/bodiless-demo/bodiless.env.config.js
@@ -1,6 +1,18 @@
 /* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
+
+const getCurrentGitBranch = () => {
+  const cmd = 'git rev-parse --abbrev-ref HEAD';
+  try {
+    const br = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+    return br;
+  } catch (e) {
+    console.warn('Could not determine current branch');
+    return '';
+  }
+}
 
 module.exports = {
   configure: async (defaultConfig) => {
@@ -11,6 +23,11 @@ module.exports = {
         const searchConf = JSON.parse(fs.readFileSync(confFilePath, 'utf8'));
         finalConfig.BODILESS_SEARCH_PARAMS = JSON.stringify(searchConf);
       }
+    }
+    if (getCurrentGitBranch() === 'main') {
+      finalConfig.BODILESS_BACKEND_SAVE_ENABLED = '0';
+    } else {
+      finalConfig.BODILESS_BACKEND_COMMIT_ENABLED = '1';
     }
     return finalConfig;
   },
