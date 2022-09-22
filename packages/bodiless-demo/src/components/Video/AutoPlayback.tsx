568757917f91a7acc9ae20224135bd6dce42f44d
hvanyo <hvanyo@its.jnj.com>
2022-09-22 17:14:40 -0500

    add checks for unmounting & dom errors

--- packages/bodiless-demo/src/components/Video/AutoPlayback.tsx
+++ packages/bodiless-demo/src/components/Video/AutoPlayback.tsx
@@ -7,12 +7,27 @@ const useVideoAutoPlayback = options => {
   const containerRef = useRef<HTMLDivElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
 
+  // catching the unmounted error
+  if (!containerRef || !videoRef) return [null, null];
+
   // @ts-ignore:  TS7006: Parameter 'entries' implicitly has an 'any' type.
   const cb = entries => {
     const [entry] = entries;
     if (videoRef.current) {
-      if (entry.isIntersecting) videoRef.current!.play();
-      else videoRef.current!.pause();
+      if (entry.isIntersecting) {
+        // https://developer.chrome.com/blog/play-request-was-interrupted/
+        const playPromise = videoRef.current!.play();
+        if (playPromise !== undefined) {
+          playPromise.then(_ => {
+            // Automatic playback started!
+            // Show playing UI.
+          })
+            .catch(error => {
+            // Auto-play was prevented
+            // Show paused UI.
+            });
+        }
+      } else videoRef.current!.pause();
     }
   };
   useEffect(() => {
@@ -22,6 +37,7 @@ const useVideoAutoPlayback = options => {
       if (containerRef.current) observer.unobserve(containerRef.current);
     };
   }, [containerRef, options]);
+
   return [containerRef, videoRef];
 };
 
