86dd86f5bec63cb054f5c099230a7f040d8172d4
Chris Oden <coden@its.jnj.com>
2022-10-06 18:21:21 -0400

    Remove eyebrow from vertical card

--- packages/bodiless-demo/src/components/Card/index.ts
+++ packages/bodiless-demo/src/components/Card/index.ts
@@ -84,6 +84,7 @@ const asVideoCard = asCardToken(
 const asVideoVerticalCard = asCardToken(
   VideoCardDefault,
   vitalCard.WithVerticalOrientation,
+  vitalCard.WithNoEyebrow,
   vitalCard.WithNoTitle,
 );
 
