5d598aeb6e6cbfcda8949fd82c1083f49add72a8
Chris Oden <coden@its.jnj.com>
2022-10-10 06:37:44 -0400

    Clean up ecosystem.config.js

--- edit/ecosystem.config.js
+++ edit/ecosystem.config.js
@@ -1,32 +1,28 @@
-const fs = require('fs');
-
-const appVolume = () => process.env.APP_VOLUME || '/app/volume';
-const gatsbyPort = () => process.env.PORT || 8888;
-const siteDirName = () => process.env.APP_SITE_DIR_NAME || 'test-site';
-const backend = () => {
-  const backendRelative = 'node_modules/@bodiless/backend/src/server.js';
-  const siteLevelBackend = `${appVolume()}/root/sites/${siteDirName()}/${backendRelative}`;
-  return fs.existsSync(siteLevelBackend)
-    ? siteLevelBackend : `${appVolume()}/root/${backendRelative}`;
-};
+/**
+ * @file
+ * Defines processes to run on p.sh
+ */
+const appVolume = process.env.APP_VOLUME || '/app/volume';
+const gatsbyPort = process.env.PORT || 8888;
+const siteDirName = process.env.APP_SITE_DIR_NAME || 'bodiless-demo';
 
 module.exports = {
   apps: [
     {
       name: 'frontend',
-      cwd: `${appVolume()}/root/sites/${siteDirName()}`,
+      cwd: `${appVolume}/root/sites/${siteDirName}`,
       script: 'npm',
-      args: `run dev-frontend -- --port ${gatsbyPort()}`,
+      args: `run dev-frontend -- --port ${gatsbyPort}`,
     },
     {
       name: 'backend',
-      cwd: `${appVolume()}/root/sites/${siteDirName()}`,
+      cwd: `${appVolume}/root/sites/${siteDirName}`,
       script: 'npm',
       args: 'run dev-backend',
     },
     {
       name: 'package',
-      cwd: `${appVolume()}/root/packages/${siteDirName()}`,
+      cwd: `${appVolume}/root/packages/${siteDirName}`,
       script: 'npm',
       args: 'run build:watch',
     },
