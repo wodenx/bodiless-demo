b7567ebe156dff5c83bc30108acc5d5785a7f470
Chris Oden <coden@its.jnj.com>
2022-10-14 04:55:29 -0400

    Remove content and disable refresh on loadf

--- sites/bodiless-demo/src/components/Page/index.ts
+++ sites/bodiless-demo/src/components/Page/index.ts
@@ -3,14 +3,15 @@ import { as } from '@bodiless/fclasses';
 import { vitalPage } from '@bodiless/vital-templates';
 import { asFluidToken } from '@bodiless/vital-elements';
 import { withDrupalArticleData } from './withDrupalArticleData';
-import withRefreshContentOnLoad from './withRefreshContentOnLoad';
+// import withRefreshContentOnLoad from './withRefreshContentOnLoad';
 
 // Note we define this component at site level because we need the
 // StaticQuery in './withDrupalArticleData'.  See comment there.
 const Default = asFluidToken({
   ...vitalPage.Default,
   Schema: {
-    _: as(withDrupalArticleData, withRefreshContentOnLoad, vitalPage.Default.Schema._),
+    // _: as(withDrupalArticleData, withRefreshContentOnLoad, vitalPage.Default.Schema._),
+    _: as(withDrupalArticleData, vitalPage.Default.Schema._),
   },
 });
 
